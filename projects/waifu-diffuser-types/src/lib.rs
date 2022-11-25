403536980c380c6f1cb00093239b28e4a477c793
Aster <galaster@foxmail.com>
2022-11-26 06:50:15 +0800

    🔮 Support string form of resource path

--- projects/waifu-diffuser-types/src/lib.rs
+++ projects/waifu-diffuser-types/src/lib.rs
@@ -2,7 +2,9 @@ pub use diffuser_scheduler::{DiffuserScheduler, EulerDiscreteScheduler};
 
 pub use crate::{
     errors::{DiffuserError, DiffuserErrorKind, DiffuserResult},
-    models::{clip::ClipModel, deep_dan_booru::DanBooruModel, unet::UNetModel, vae::VaeModel, DiffuserModel},
+    models::{
+        clip::ClipModel, deep_dan_booru::DanBooruModel, model_path::ResourcePath, unet::UNetModel, vae::VaeModel, DiffuserModel,
+    },
     tasks::*,
 };
 
