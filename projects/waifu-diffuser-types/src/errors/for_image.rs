92ad1c1ad78c0b156858ccb27eca8e65a03d1747
Aster <galaster@foxmail.com>
2022-11-28 00:20:40 +0800

    🔮 Move ResourcePath to third part

--- projects/waifu-diffuser-types/src/errors/for_image.rs
+++ projects/waifu-diffuser-types/src/errors/for_image.rs
@@ -1,7 +1,15 @@
 use crate::DiffuserError;
 use image::ImageError;
+use url::{ParseError, Url};
+
 impl From<ImageError> for DiffuserError {
     fn from(value: ImageError) -> Self {
         todo!()
+    }
+}
+
+impl From<ParseError> for DiffuserError {
+    fn from(value: ParseError) -> Self {
+        DiffuserError::decode_error(value.to_string())
     }
 }
--- projects/waifu-diffuser-types/src/errors/for_serde_json.rs
+++ projects/waifu-diffuser-types/src/errors/for_serde_json.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde_json::Error;
 
 use crate::DiffuserError;
@@ -5,5 +7,23 @@ use crate::DiffuserError;
 impl From<Error> for DiffuserError {
     fn from(e: Error) -> Self {
         DiffuserError::decode_error(e.to_string())
+    }
+}
+
+impl serde::ser::Error for DiffuserError {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: Display,
+    {
+        DiffuserError::custom_error(msg.to_string(), -10102)
+    }
+}
+
+impl serde::de::Error for DiffuserError {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: Display,
+    {
+        DiffuserError::decode_error(msg.to_string())
     }
 }
