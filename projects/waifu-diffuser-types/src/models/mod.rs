aa38b5231712554a199cd42ef16cf4d0c6206929
Aster <galaster@foxmail.com>
2023-03-04 23:08:38 +0800

    Add dan booru model

--- projects/waifu-diffuser-types/src/models/mod.rs
+++ projects/waifu-diffuser-types/src/models/mod.rs
@@ -1,7 +1,12 @@
+use crate::{ClipModel, DanBooruModel};
 use serde::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
 
+pub mod clip;
+pub mod deep_dan_booru;
 mod reader;
+pub mod unet;
+pub mod vae;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct DiffuserModel {
@@ -11,19 +16,12 @@ pub struct DiffuserModel {
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum ModelKind {
+    /// A ResNet model used for find prompts.
+    DeepDanBooru(Box<DanBooruModel>),
     /// A VAE model used for stable diffusion 1.
     Vae,
-    /// A VAE model used for stable diffusion 2.
-    Vae2,
     /// A U-Net model used for stable diffusion 1.
     UNet,
     /// A U-Net model used for stable diffusion 2.
     Clip(Box<ClipModel>),
-    /// A CLIP model used for stable diffusion 2.
-    Clip2,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct ClipModel {
-    name: String,
 }
